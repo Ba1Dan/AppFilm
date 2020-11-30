8be73bda465bfc887eb975dfe97d1c1624ecb320
Данияр <daniyar.baiganov@gmail.com>
2020-11-30 16:48:46 +0300

    FragmentMoviesList to FragmentMoviesDetails.kt

--- app/src/main/java/ru/baiganov/appfilm/FragmentMoviesDetails.kt
+++ app/src/main/java/ru/baiganov/appfilm/FragmentMoviesDetails.kt
@@ -4,11 +4,30 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 
 class FragmentMoviesDetails : Fragment() {
 
+    private var tvBackToMovieList:TextView? = null
+    private var listener:ClickListener? = null
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_movies_details, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        tvBackToMovieList = view.findViewById<TextView>(R.id.tv_back).apply {
+            setOnClickListener{ listener?.backToMovieList() }
+        }
+    }
+
+    fun setListener(l: ClickListener) {
+        listener = l
+    }
+
+    interface ClickListener {
+        fun backToMovieList()
     }
 }
--- app/src/main/java/ru/baiganov/appfilm/MainActivity.kt
+++ app/src/main/java/ru/baiganov/appfilm/MainActivity.kt
@@ -2,10 +2,36 @@ package ru.baiganov.appfilm
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.cardview.widget.CardView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), FragmentMoviesDetails.ClickListener {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
+        val fragmentMoviesList = FragmentMoviesList()
+
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.fl_main_activity, fragmentMoviesList)
+            commit()
+        }
+    }
+
+    fun onClickCard(view: View) {
+        val fragmentMoviesDetails = FragmentMoviesDetails()
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.fl_main_activity, fragmentMoviesDetails)
+            commit()
+        }
+    }
+
+    override fun backToMovieList() {
+       val fragmentMoviesList = FragmentMoviesList()
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.fl_main_activity, fragmentMoviesList)
+            commit()
+        }
     }
 }
